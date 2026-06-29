3c9d8878594d91b7bfb27882b583c74b09a1f3ac
Konnecta Groupe <groupekonnecta@gmail.com>
2026-06-29 03:01:41 +0100

    Add social groups config

---
+++ lib/social-groups.ts
@@ -0,0 +1,22 @@
+// Groupes Facebook où l'utilisateur diffuse manuellement ses annonces.
+// (Meta a supprimé l'API de publication dans les groupes le 22/04/2024 →
+//  diffusion assistée : texte prêt + ouverture du groupe, l'utilisateur colle.)
+export const FB_GROUPS: { url: string }[] = [
+  { url: "https://web.facebook.com/groups/479966003683706" },
+  { url: "https://web.facebook.com/groups/27224878793801023" },
+  { url: "https://web.facebook.com/groups/2626356151093435" },
+  { url: "https://web.facebook.com/groups/2244030626135022" },
+  { url: "https://web.facebook.com/groups/2155758281823727" },
+  { url: "https://web.facebook.com/groups/1968570757106517" },
+  { url: "https://web.facebook.com/groups/1963851664242508" },
+  { url: "https://web.facebook.com/groups/1791635568882396" },
+  { url: "https://web.facebook.com/groups/1691257142106366" },
+  { url: "https://web.facebook.com/groups/1687771309160199" },
+  { url: "https://web.facebook.com/groups/1587955429521634" },
+  { url: "https://web.facebook.com/groups/1557603289168345" },
+  { url: "https://web.facebook.com/groups/1042112691760861" },
+  { url: "https://web.facebook.com/groups/2369163863243473" },
+  { url: "https://web.facebook.com/groups/amazonkidleebooks" },
+  { url: "https://web.facebook.com/groups/671202853010470" },
+  { url: "https://web.facebook.com/groups/5261881627156283" },
+];
